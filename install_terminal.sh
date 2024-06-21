e9f3542cc0d8e4d53ec8f77d86a3e01c2e051e38
Nikko Yu <nikko.yu@thinkcol.com>
2024-06-21 10:34:42 +0000

    add: .zshrc and oh-my-zsh, autosuggestions

--- install_terminal.sh
+++ install_terminal.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 sudo apt install zsh -y
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# install auto-suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# copy existing configurations to ~/.zshrc
+cp .zshrc ~/.zshrc
